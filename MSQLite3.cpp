414c91773bfac7caedc2b31078a7a55fd4369755
Marek Stancik <43235061+MarekStancik@users.noreply.github.com>
2019-11-10 22:18:25 +0100

    first commit

---
+++ MSQLite3.cpp
@@ -0,0 +1,208 @@
+#include "pch.h"
+#include "MSQLite3.h"
+#include <stdexcept>
+
+//------------------------ResultSet-----------------------------//
+
+ResultSet::ResultSet()
+{
+	iterator = container.begin();
+}
+
+void ResultSet::addRecord(int count, const char** row, const char** cols)
+{
+	if (count)
+	{
+		Container::value_type icontain;
+		for (int i = 0; i < count; i++)
+			icontain.insert({ cols[i],(row[i] ? row[i] : "") });
+		addRecord(std::move(icontain));
+	}
+}
+
+void ResultSet::addRecord(Container::value_type&& record)
+{
+	container.emplace_back(std::move(record));
+	iterator = container.begin();
+}
+
+ResultSet::operator bool()
+{
+	return container.size() > 0 && iterator != container.end();
+}
+
+bool ResultSet::next()
+{
+	if (iterator != container.end())
+		iterator = std::next(iterator, 1);
+	return iterator != container.end();
+}
+
+size_t ResultSet::count()
+{
+	return container.size();
+}
+
+//------------------------SQLite3-----------------------------//
+
+SQLite3::SQLite3(const char* dbPath, const char* createStmt) :errMsg(nullptr), db(nullptr)
+{
+	result = sqlite3_open(dbPath, &db);
+	isOpened = result == SQLITE_OK;
+
+	//If statement to create database was provided
+	if (createStmt)
+		result = sqlite3_exec(db, createStmt, [](void* data, int count, char** row, char** columns)->int { return 0; }, 0, &errMsg);
+
+	if (result != SQLITE_OK)
+		throw SQLite3Error(std::string("Database can't be initialized: \nResult: ") + std::to_string(result) + "\t" + errMsg);
+}
+
+SQLite3::~SQLite3()
+{
+	if (errMsg != nullptr)
+		sqlite3_free(errMsg);
+	if (isOpened)
+		sqlite3_close(db);
+}
+
+bool SQLite3::isPrepared() const
+{
+	return isOpened;
+}
+
+int SQLite3::prepare()
+{
+	return 0;
+}
+
+const char* SQLite3::error() const
+{
+	return errMsg ? errMsg : "NULL";
+}
+
+sqlite_int64 SQLite3::lastId() const
+{
+	return isPrepared() ? sqlite3_last_insert_rowid(db) : -1;
+}
+
+void SQLite3::execute(const char* sql)
+{
+	if (sql)
+	{
+		result = sqlite3_exec(db, sql, [](void* data, int count, char** row, char** columns)->int {return 0; }, 0, &errMsg);
+		if (result != SQLITE_OK)
+			throw SQLite3Error(errMsg);
+	}
+	else
+		throw SQLite3Error("No sql parameter");
+}
+
+ResultSet SQLite3::executeQuery(const char* sql)
+{
+	if (sql)
+	{
+		ResultSet retval;
+		result = sqlite3_exec(db, sql, [](void* data, int count, char** row, char** columns)->int
+			{
+				ResultSet* rs = (ResultSet*)data;
+				rs->addRecord(count, (const char**)row, (const char**)columns);
+				return 0;
+			}, &retval, &errMsg);
+		if (result != SQLITE_OK)
+			throw SQLite3Error(errMsg);
+		return retval;
+	}
+	else
+		throw SQLite3Error("No sql parameter");
+}
+
+void SQLite3::beginTransaction()
+{
+	result = sqlite3_exec(db, "BEGIN TRANSACTION", 0, 0, &errMsg);
+
+	if (result != SQLITE_OK)
+		throw SQLite3Error(errMsg);
+}
+
+void SQLite3::endTransaction()
+{
+	result = sqlite3_exec(db, "END TRANSACTION", 0, 0, &errMsg); 
+	
+	if (result != SQLITE_OK)
+		throw SQLite3Error(errMsg);
+}
+
+std::string SQLite3::toString(const std::tm& date)
+{
+	std::stringstream ss("");
+	ss << std::put_time(&date, "%Y-%m-%d");
+	return ss.str();
+}
+
+std::string SQLite3::toStringM(const std::tm& date)
+{
+	std::stringstream ss("");
+	ss << std::put_time(&date, "%Y-%m");
+	return ss.str();
+}
+
+PreparedStatement& PreparedStatement::reset()
+{
+	rc = sqlite3_step(stmt);
+	rc = sqlite3_clear_bindings(stmt);
+	rc = sqlite3_reset(stmt);
+	return *this;
+}
+
+PreparedStatement& PreparedStatement::execute()
+{
+	do {
+		rc = sqlite3_step(stmt);
+	} while (rc == SQLITE_ROW);
+
+	if (rc != SQLITE_DONE)
+		throw SQLite3Error(sqlite3_errmsg(db));
+
+	return *this;
+}
+
+ResultSet PreparedStatement::executeQuery()
+{
+	ResultSet rs;
+	while (sqlite3_step(stmt) == SQLITE_ROW) // While query has result-rows.
+	{ 
+		//  Iterate all columns and push column - name value pair to record
+		ResultSet::Container::value_type record;
+		for (int colIndex = 0; colIndex < sqlite3_column_count(stmt); ++colIndex) 
+			record.insert({ sqlite3_column_name(stmt, colIndex), (const char*)sqlite3_column_text(stmt, colIndex) });
+
+		rs.addRecord(std::move(record));
+	}
+	return rs;
+}
+
+PreparedStatement::PreparedStatement(PreparedStatement&& ps)
+{
+	*this = std::move(ps);
+}
+
+PreparedStatement& PreparedStatement::operator=(PreparedStatement&&ps)
+{
+	if (this->stmt)
+		sqlite3_finalize(this->stmt);
+
+	this->db = ps.db;
+	this->rc = ps.rc;
+	this->stmt = ps.stmt;
+	this->paramCount = ps.paramCount;
+	ps.db = nullptr;
+	ps.stmt = nullptr;
+	return *this;
+}
+
+PreparedStatement::~PreparedStatement()
+{
+	if(stmt)
+		rc = sqlite3_finalize(stmt);
+}
---
+++ MSQLite3.h
@@ -0,0 +1,309 @@
+#ifndef MSQLite3H
+#define MSQLite3H
+#include "sqlite3.h"
+#include <ctime>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+
+//-------------Forward declarations-------------
+
+//ResultSet class is Object that is beeing returned on executeQuery request
+//This class is basically container that maps returned column names to its values
+//To get value from ResultSet call get<> method and provide column name
+//To read all returned rows iterate over ResultSet by method: next
+//if there is no row left operator bool returns false
+class ResultSet;
+
+//PreparedStatement class is used to execute queries that needs prepared statements
+//To create PreparedStatement you need to have SQLite3 class initialized and call function createPreparedStatement
+//You insert the query for example like this : INSERT INTO table(col1,col2) VALUES(?,?) where ? is position of parameters.
+//Then you need to bind values to that parameters so call preparedStatement.bind(value1,value2), 
+//note that you have to bind same number of values as  is number of question marks, this bindable parameters can be also provided in constructor
+//To execute command call execute() method
+//To execute query which returns result set call executeQuery() method
+//To reset the parameters but keep the query call reset() method - this can be used when you want to call same query multiple times with differen parameters
+class PreparedStatement;
+
+//SQLite3 class is the main connection to the database, this class handles the communication with database
+//For better performance it is recommended to call beginTransaction() before and call endTransaction() after queries
+//You can also execute raw queries by execute() and executeQuery() methods
+class SQLite3;
+
+//Error class for all SQLite3 Errors
+class SQLite3Error;
+
+class SQLite3Error : public std::runtime_error {
+public:
+	using std::runtime_error::runtime_error;
+};
+
+class ColumnNotFound : public SQLite3Error {
+public:
+	ColumnNotFound(const std::string& colName)
+		:SQLite3Error("There is no column by name: '" + colName + "' in current resultset")
+	{}
+};
+
+class ResultSet
+{
+public:
+	//Container type to store and map returned rows/cols
+	typedef std::vector<std::unordered_map<std::string, std::string>> Container;
+private:
+	//Internal container object
+	Container container;
+
+	//Iterator object for container
+	Container::iterator iterator;
+public:
+	//Constructor - does nothing special
+	ResultSet();
+
+	//Add record to the result set
+	void addRecord(int count, const char** row, const char** cols);
+	void addRecord(Container::value_type&& record);
+	
+	//Returns true if container is still iterable
+	operator bool();
+	
+	//Moves to the next row and returns false if it reached end
+	bool next();
+
+	//Return number of rows in resultset
+	size_t count();
+
+	//Return value for current row and given column name
+	//Throws if no such column exist
+	template<typename T>
+	T get(const std::string& name)
+	{
+		T rval{0};
+		auto it = iterator->find(name);
+		if (it != iterator->end())
+		{
+			std::istringstream ss(it->second);
+			ss >> rval;
+		}
+		else
+			throw ColumnNotFound(name);
+		return rval;
+	}
+};
+
+//explicit specialization for std::string to take also a whitespace
+template<>
+inline std::string ResultSet::get(const std::string& name)
+{
+	std::string rval{};
+	auto it = iterator->find(name);
+	if (it != iterator->end())
+	{
+		std::istringstream ss(it->second);
+		std::getline(ss, rval);
+	}
+	else
+		throw ColumnNotFound(name);
+	return rval;
+}
+
+
+//explicit specialization for std::tm to parse date correctly
+template<>
+inline std::tm ResultSet::get(const std::string& name)
+{
+	std::tm rval{};
+	auto it = iterator->find(name);
+	if (it != iterator->end())
+	{
+		std::stringstream ss(it->second);
+		ss >> std::get_time(&rval, "%Y-%m-%d %H:%M:%S");
+	}
+	else
+		throw ColumnNotFound(name);
+	return rval;
+}
+
+
+class PreparedStatement
+{
+private:
+	//Pointer to underlying db connection of statement
+	//Memory management is handled in SQLite3 class
+	sqlite3* db;
+
+	//Pointer to statment object, acquired in constructor by sqlite3_prepare_v3 method
+	//Freed by sqlite3_finalize method in destructor or during move operation
+	sqlite3_stmt* stmt;
+
+	//Result of sql operations
+	int rc;
+
+	//Count of parameters found in query
+	int paramCount;
+
+	//Prepare parameter of floating point type
+	template<typename T>
+	void prepareParam(const T& param, const int index
+		,typename std::enable_if<std::is_floating_point<T>::value>::type* = 0){
+		rc = sqlite3_bind_double(stmt, index, param);
+	}
+
+	//Prepare parameter of integral type
+	template<typename T>
+	void prepareParam(const T& param, const int index
+		, typename std::enable_if<std::is_integral<T>::value && !std::is_same_v<T, sqlite_int64>>::type* = 0){
+		rc = sqlite3_bind_int(stmt, index, param);
+	}
+
+	//Prepare parameter of int64 type
+	template<typename T>
+	void prepareParam(const T& param, const int index
+		, typename std::enable_if<std::is_same_v<T,sqlite_int64>>::type* = 0) {
+		rc = sqlite3_bind_int64(stmt, index, param);
+	}
+
+	//Prepare parameter of const char* type
+	void prepareParam(const char* param, const int index){
+		rc = sqlite3_bind_text(stmt, index, param, -1, SQLITE_STATIC);
+	}
+
+	//Prepare parameter of string type
+	void prepareParam(const std::string& param, const int index) {
+		rc = sqlite3_bind_text(stmt, index, param.c_str(), param.length(), SQLITE_STATIC);
+	}
+
+	//Prepare parameter of char type
+	void prepareParam(const char& param, const int index) {
+		rc = sqlite3_bind_text(stmt, index, &param, 1, SQLITE_STATIC);
+	}
+
+
+	//Constructor of object is in private section
+	//It can be only called by SQLite3 component
+	//Params: [db] underlying database pointer
+	//[query] string query to prepare
+	//[args] parameters for query
+	template<typename ...Args>
+	PreparedStatement(sqlite3* db, const std::string& query, Args&&... args)
+		:
+		rc(SQLITE_OK)
+		,db(db)
+		,stmt(nullptr)
+		,paramCount(std::count(query.begin(), query.end(), '?'))
+	{	
+		//Prepares query and allocate stmt object
+		rc = sqlite3_prepare_v3(db, query.c_str(),query.length(), 0, &stmt, 0);
+
+		if (rc != SQLITE_OK)
+			throw SQLite3Error(sqlite3_errmsg(db));
+		
+		//Bind only if there are some arguments
+		if(sizeof...(args) > 0)
+			bind(std::forward<Args>(args)...);
+	}
+
+	//So SQLite3 component can create PreparedStatement
+	friend class SQLite3;
+public:
+
+	//Binds given arguments to sql query
+	//Count of arguments have to be the same as the number of questionmarks in query
+	template<typename ...Args>
+	PreparedStatement& bind(Args&&... args) {
+		//Check correct size
+		if (sizeof...(args) != paramCount)
+			throw SQLite3Error("Count of arguments does not equal count of questionmarks");
+
+		//Prepare all parameters from parameter pack
+		int index = 0;
+		using expander = int[];
+		(void)expander {
+			0, (void(prepareParam(std::forward<Args>(args), ++index)), 0)...
+		};
+
+		if (rc != SQLITE_OK)
+			throw SQLite3Error(sqlite3_errmsg(db));
+		return *this;
+	}
+
+	//Resets the parameters but keeps the query
+	//Is used to fill the same query with new params
+	PreparedStatement& reset();
+
+	//Executes statement
+	PreparedStatement& execute();
+	
+	//Executes query
+	ResultSet executeQuery();
+
+	//Deleted because of stmt memory management
+	PreparedStatement(const PreparedStatement&) = delete;
+	PreparedStatement& operator=(const PreparedStatement&) = delete;
+
+	//Only allowed move operations
+	PreparedStatement(PreparedStatement&&);
+	PreparedStatement& operator=(PreparedStatement&&);
+	~PreparedStatement();
+};
+
+class SQLite3
+{
+private:
+	//Db conneciton pointer
+	sqlite3* db;
+
+	//Error message pointer
+	char* errMsg;
+
+	//Results of sqlite3 calls
+	int result;
+
+	//flag to signalize if db is opened
+	bool isOpened;
+public:
+	//Takes as parameter path to database and create statement
+	SQLite3(const char* dbPath, const char* createStmt = nullptr);
+
+	//closes and dealocates db
+	~SQLite3();
+
+	//return true if db is connected oterwise false
+	bool isPrepared() const;
+
+	//Tries to connect db 
+	//not implemented
+	int prepare();
+
+	//returns last error message
+	const char* error() const;
+
+	//returns last inserted id
+	sqlite_int64 lastId() const;
+
+	//Following 2 operations are not recommended for security reasons
+	//execute raw sql query
+	void execute(const char* sql);
+
+	//Executes raw sql query andreturn resultset of that query
+	ResultSet executeQuery(const char* sql);
+
+	//Creates prepared statement for this db connection
+	template<typename ...Args>
+	PreparedStatement createPreparedStatement(const std::string& query,Args&&... args){
+		return PreparedStatement(db,query, std::forward<Args>(args)...);
+	}
+
+	void beginTransaction();
+	void endTransaction();
+
+	//converts date to date string in format %y-%m-%d
+	static std::string toString(const std::tm& date);
+
+	//converts date to date string in format %y-%m
+	static std::string toStringM(const std::tm& date);
+};
+
+#endif
